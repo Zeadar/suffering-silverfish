7263b78b3e7d32b92a6e4094987bfdbdc32d9ea1
Zeadar <siiigsonik@gmail.com>
2025-01-16 21:51:59 +0100

    vite serve & build difference config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,22 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-export default defineConfig(() => {
-    return {
-        plugins: [react()],
-        server: {
-            proxy: {
-                "/api": {
-                    target: "http://127.0.0.1:7878",
-                    changeOrigin: true,
+export default defineConfig(({ command }) => {
+    if (command == "serve") {
+        return {
+            plugins: [react()],
+            server: {
+                proxy: {
+                    "/api": {
+                        target: "http://127.0.0.1:7878",
+                        changeOrigin: true,
+                    },
                 },
             },
-        },
+        }
+    } else {
+        return {
+            plugins: [react()],
+        }
     }
 })
